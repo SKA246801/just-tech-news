0c59c74f854a13b809877f8892418650bcce796a
Sebastian <sebastianalvarado246801@gmail.com>
2021-06-20 09:41:46 -0500

    finished lesson 5

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { Post, User, Vote, Comment } = require('../../models')
 const sequelize = require('../../config/connection')
+const withAuth = require('../../utils/auth')
 
 //get all users
 router.get('/', (req, res) => {
@@ -63,7 +64,7 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
       title: req.body.title,
@@ -77,7 +78,7 @@ router.post('/', (req, res) => {
     })
 })
 
-router.put('/upvote', (req, res) => {
+router.put('/upvote', withAuth, (req, res) => {
     // make sure the session exists first
   if (req.session) {
     // pass session id along with all destructured properties on req.body
@@ -90,7 +91,7 @@ router.put('/upvote', (req, res) => {
   }
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
       {
         title: req.body.title
@@ -114,7 +115,7 @@ router.put('/:id', (req, res) => {
     })
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
       where: {
         id: req.params.id
